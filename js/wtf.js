230aeaca0859cfafe7c8b04605309adb7a17ea1d
seb-montana <seb-montana@users.noreply.github.com>
2019-07-22 11:41:08 +0200

    - add some utils javascript functions

--- js/wtf.js
+++ js/wtf.js
@@ -1,6 +1,5 @@
 /**
- Theme Name: Woody
- Theme URI: http://lab.studio-montana.com/woody-theme/
+ What The Fuck Framework
  Author: Studio Montana (Sebastien Chandonay / Cyril Tissot)
  Author URI: http://www.studio-montana.com
  License: GNU General Public License v2 or later
@@ -10,6 +9,79 @@
  Use it to make something cool, have fun, and share what you've learned with others.
  */
 
+
+	
+/**
+ * Set Cookie
+ * @param cname
+ * @param cvalue
+ * @param exdays
+ * @returns
+ */
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * Retrieve Cookie
+ * @param cname
+ * @returns
+ */
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function isFloat(n) {
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isInt(n) {
+	return !isNaN(parseInt(n)) && isFinite(n);
+}
+
+function isset(variable) {
+	if (typeof (variable) == undefined || variable == null) {
+		return false;
+	}
+	return true;
+}
+
+function empty(variable) {
+	if (!isset(variable) || variable == '') {
+		return true;
+	}
+	return false;
+}
+
+function indexOf(value, array) {
+	var res = -1;
+	if (!empty(array)) {
+		for ( var i = 0; i < array.length; i++) {
+			if (array[i] === value)
+				res = i;
+		}
+	}
+	return res;
+}
+
+/**
+ * JQuery
+ */
 (function($) {
 
 	/**
